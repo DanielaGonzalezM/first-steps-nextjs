1af5fd73808c6de4ffcb3ea9930c221c14961849
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-21 13:56:07 -0300

    link next

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,36 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  {
+    path: "/about",
+    text: "About",
+  },
+  {
+    path: "/pricing",
+    text: "Pricing",
+  },
+  {
+    path: "/contact",
+    text: "Contact",
+  },
+];
 
 export const Navbar = () => {
-    console.log("Navbar creado");
+  console.log("Navbar creado");
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+      <Link className="mr-2 flex items-center" href="/">
+        <HomeIcon className="mr-2" />
         <span>Home</span>
+      </Link>
 
-        <div className="flex flex-1"></div>
-        <a className="mr-2" href="/about">About</a>
-        <a className="mr-2" href="/pricing">Pricing</a>
-        <a className="mr-2" href="/contact">Contact</a>
+      <div className="flex flex-1"></div>
+      {
+        navItems.map(navItem=>(
+            <Link key={navItem.path} className="mr-2" href={navItem.path}>{navItem.text}</Link>
+        ))
+      }
     </nav>
-  )
-}
+  );
+};
