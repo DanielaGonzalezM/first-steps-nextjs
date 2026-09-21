7a63c70a4c853e4203d3078509cd216e7fb72270
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-21 14:55:24 -0300

    usePathname - ActiveLink

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { HomeIcon } from "@primer/octicons-react";
 import Link from "next/link";
+import ActiveLink from "../active-link/ActiveLink";
 
 const navItems = [
   {
@@ -26,11 +27,9 @@ export const Navbar = () => {
       </Link>
 
       <div className="flex flex-1"></div>
-      {
-        navItems.map(navItem=>(
-            <Link key={navItem.path} className="mr-2" href={navItem.path}>{navItem.text}</Link>
-        ))
-      }
+      {navItems.map((navItem) => (
+        <ActiveLink key={navItem.path} {...navItem} />
+      ))}
     </nav>
   );
 };
